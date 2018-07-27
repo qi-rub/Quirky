4e5d897a42d4c0ac32051df3697a32d038b567b9
Michael Walter <michael.walter@gmail.com>
2018-07-26 21:25:27 -0400

    Rename

--- src/ui/DisplayedToolbox.js
+++ src/ui/DisplayedToolbox.js
@@ -77,7 +77,7 @@ class DisplayedToolbox {
         let groups = [...this._originalGroups];
         for (let i = 0; i < Math.max(1, customGateSet.gates.length); i += this.groupHeight*3) {
             let group = {
-                hint: "Custom Rotations",
+                hint: "My Rotations",
                 gates: [undefined, undefined]
             };
             for (let j = 0; j < this.groupHeight*3 && i + j < customGateSet.gates.length; j++) {
