0d9fd567904c945371a51f293c87c580e2fdd2ee
Michael Walter <michael.walter@gmail.com>
2018-11-15 19:45:06 +0100

    Start with two qubits to avoid confusion.

--- src/profile/Quest3.js
+++ src/profile/Quest3.js
@@ -5,7 +5,7 @@ Config.LONG_TITLE = 'The Quirky Quantum Simulator';
 Config.LONG_SUBTITLE = 'Quest 3: Quantum composer';
 
 Config.GATESET = 'Quest 3';
-Config.MIN_WIRE_COUNT = 1;
+Config.MIN_WIRE_COUNT = 2;
 Config.MAX_WIRE_COUNT = 10;
 
 export {Config}
